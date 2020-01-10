d875d629762867f4bed8dca944a55e360942ff84
charlesson95 <32780510+charlesson95@users.noreply.github.com>
2020-01-10 00:30:26 -0600

    charles was here

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to our OurSpace
+          Welcome to our OurSpace. An C2C platform for renting storage spaces.
           <br />
           Edit <code>src/App.js</code> and save to reload.
         </p>
