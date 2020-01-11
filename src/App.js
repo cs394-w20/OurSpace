6b80dd75c3ec59166aa27c108346737af659c4e4
charlesson95 <32780510+charlesson95@users.noreply.github.com>
2020-01-11 16:21:33 -0600

    initial listing implementation

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import "rbx/index.css";
+// import Icon from '@material-ui/core/Icon';
+
+import parking from "./assets/local_parking-24px.svg";
+import elevator from "./assets/elevator.svg";
+// import ramp from "./assets/ramp.svg";
+
+
+
+import { Card, Content, List, Image, Media, Title, Button } from "rbx";
+
+
+
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to OurSpace. A C2C platform for renting storage spaces for cheap.
-          <br />
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input></input>
+      <StorageCard></StorageCard>
     </div>
+
+
+
+
   );
-}
+};
+
+const StorageCard = ({storage_option}) => (
+  <div>
+    <Card>
+    <Card.Image>
+      <Image.Container size="4by3">
+        <Image src="https://i.pinimg.com/originals/6a/7c/fc/6a7cfc513ee281ac19ed5b25f17a9a5a.jpg" />
+      </Image.Container>
+    </Card.Image>
+
+    <Card.Content>
+      <Content>
+        Mudd Garage <br/>
+        $ 35 / night <br/>
+        5 miles away <br/>
+
+          <img src={parking}/>
+          <img style={{width:'24px'}}src={elevator}/>
+          {/* <img src={ramp}/> */}
+        
+
+      </Content>
+    </Card.Content>
+  </Card>
+  </div>
+);
+  
+
 
 export default App;
