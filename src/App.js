5d64070601a1bad52c1af37bf38e689df1ceaec5
charlesson95 <32780510+charlesson95@users.noreply.github.com>
2020-01-12 04:36:47 -0600

    updated card design

--- src/App.js
+++ src/App.js
@@ -19,29 +19,60 @@ function App() {
 
 const StorageCard = ({listId}) => {
   return (
-    <div style={{width:"90%", margin:"Auto", paddingTop: '30px'}}>
-      <Card>
+    <div style={{width:"90%", margin:"Auto", paddingTop: '20px'}}>
+      <Card style={{borderRadius:"8px"}}>
       <Card.Image>
         <Image.Container size="4by3">
-          <Image src="https://i.pinimg.com/originals/6a/7c/fc/6a7cfc513ee281ac19ed5b25f17a9a5a.jpg"/>
+          <Image src="https://i.pinimg.com/originals/6a/7c/fc/6a7cfc513ee281ac19ed5b25f17a9a5a.jpg" style={{borderRadius:"8px"}}/>
         </Image.Container>
       </Card.Image>
 
-      <Card.Content>
+      <Card.Content style={{padding:"10px"}}>
         <table style={{width:"100%"}}>
           <tr>
-            <th>
-              <div style={{fontSize:'24px', textAlign:"left", width:"80%"}}>Mudd Garage <span style={{fontSize:"15px"}}>(&#9733;4.37)</span></div>
-              <div style={{fontSize:'16px', textAlign:"left", width:"20%", fontWeight:"normal"}}><span style={{fontWeight:"bold"}}>$2</span>/ night</div>
-            </th>
-            <th style={{fontSize:'16px', textAlign:"right", float:"right", fontWeight:"normal"}}>
-              <div style={{fontSize:'16px', textAlign:"right", float:"right", fontWeight:"normal"}}>5 miles away</div> <br></br>
+            <th style={{width:"60%"}}>
+              <div style={{fontSize:'14px', textAlign:"left", fontWeight:"normal"}}>
+                <span style={{textTransform:'uppercase', fontWeight:"500", border:"1px solid black", borderRadius:"4px", padding:'4px', paddingTop:'0px', paddingBottom:'0px'}}>Large</span>
+                <span style={{paddingLeft:"4px"}}>Entire Basement</span>
 
-              <img src={parking}/>
-              <img style={{width:'24px'}}src={elevator}/>
+
+              </div>
+            </th>
+            <th style={{width:"40%", textAlign:"right", fontWeight:"normal", fontSize:"15px"}}>
+                
+
+                <div style={{display:'inline-block'}}>
+                  <span style={{color:'red'}}>&#9733;</span>4.37
+                  <span style={{fontSize:"13px", paddingLeft:"2px"}}>(32)</span>
+                </div>
             </th>
           </tr>
         </table>
+
+          <div style={{fontSize:'24px', textAlign:"left", width:"100%", lineHeight:'28px'}}>Mudd Garage
+          <div style={{display:'inline-block', float:"right", marginTop:'5px'}}>
+            
+            <img style={{height:'22px'}} src={elevator}/>
+            <img style={{height:'22px'}} src={parking}/>
+          </div>
+          
+          </div>
+          
+
+          <table style={{width:"100%", marginTop:'10px'}}>
+          <tr>
+            <th style={{width:"60%"}}>
+              <div style={{fontSize:'20px', textAlign:"left", fontWeight:"normal"}}>
+                <span style={{fontWeight:"bold"}}>$2</span>/night
+              </div>
+            </th>
+            <th style={{width:"40%", textAlign:"right", fontWeight:"normal", fontSize:"15px"}}>
+              <div style={{fontSize:'16px', textAlign:"right", float:"right", fontWeight:"normal"}}>5 miles away</div>
+              <br></br>
+            </th>
+          </tr>
+        </table>
+        
       </Card.Content>
     </Card>
     </div>
