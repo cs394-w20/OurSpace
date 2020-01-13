b1b457cf7e5a0fd1d3d54c2eddcb6d510c57abed
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2020-01-13 16:41:26 -0600

    Reorganized classes, icons, and images into assets folder. Integrated Test Data into JSON file to more accurately mimic database behavior.

--- src/App.js
+++ src/App.js
@@ -2,80 +2,13 @@ import React, { useState, useContext } from "react";
 import "./App.css";
 import "rbx/index.css";
 
-import parking from "./assets/local_parking-24px.svg";
-import elevator from "./assets/elevator.svg";
-// import ramp from "./assets/ramp.svg";
+import parking from "./assets/icons/local_parking-24px.svg";
+import elevator from "./assets/icons/elevator.svg";
+// import ramp from "./assets/icons/ramp.svg";
+
+import TestData from "./assets/data/listings.json";
 
 import { Card, Image, Column, Title, Modal, Content } from "rbx";
-
-class Location {
-  constructor(street, city, state, country, zip, geodata) {
-    this.street = street;
-    this.city = city;
-    this.state = state;
-    this.country = country;
-    this.zip = zip;
-    this.geodata = geodata;
-  }
-}
-
-class Geodata {
-  constructor(type, coordinates) {
-    this.type = type;
-    this.coordinates = coordinates;
-  }
-}
-
-class Coordinates {
-  constructor(latitude, longitude) {
-    this.latitide = latitude;
-    this.longitude = longitude;
-  }
-}
-
-class Size {
-  constructor(length, width, height) {
-    this.length = length;
-    this.width = width;
-    this.height = height;
-  }
-}
-
-class Attributes {
-  constructor(hasLock, hasParking, hasElevator, hasRamp) {
-    this.hasLock = hasLock;
-    this.hasParking = hasParking;
-    this.hasElevator = hasElevator;
-    this.hasRamp = hasRamp;
-  }
-}
-
-class Listing {
-
-  constructor(nameString, location, size, time, attributes, image, price) {
-    this.nameString = nameString;
-    this.location = location;
-    this.size = size;
-    this.time = time;
-    this.attributes = attributes;
-    this.image = image;
-    this.price = price;
-  }
-}
-
-const TestData = [new Listing("Old Orchard Apt", new Location("10104 Old Orchard Ct", "Skokie", "IL", "USA", 60076, new Geodata("point", new Coordinates(42.063752, -87.744318))), new Size(10, 15, 6), new Date(2020, 5, 10, 12, 0, 0, 0), new Attributes(false, true, false, false), "./assets/Images/apt.jpg", 5.00),
-new Listing("The Best Closet", new Location("160 Steeples Blvd", "Indianapolis", "IN", "USA", 46222, new Geodata("point", new Coordinates(39.767587, -86.212728))), new Size(2, 2, 8), new Date(2020, 1, 24, 6, 30, 0, 0), new Attributes(false, false, true, false), "./assets/Images/closet.jpg", 10.50),
-new Listing("Under The Sink", new Location("1600 Monticello Ave", "Norfolk", "VA", "USA", 23510, new Geodata("point", new Coordinates(36.864039, -76.285274))), new Size(4, 2, 2), new Date(2020, 2, 6, 15, 0, 0, 0), new Attributes(false, true, true, false), "./assets/Images/underthesink.jpg", 20.75),
-new Listing("Spare Bedroom", new Location("737 Colfax St", "Evanston", "IL", "USA", 60201, new Geodata("point", new Coordinates(42.060989, -87.681330))), new Size(10, 10, 10), new Date(2021, 11, 19, 20, 30, 0, 0), new Attributes(false, true, false, false), "./assets/Images/sparebedroom.jpg", 7.50),
-new Listing("Cellar", new Location("4065 Dunbarton Cir", "San Ramon", "CA", "USA", 94583, new Geodata("point", new Coordinates(37.757932, -121.952262))), new Size(20, 10, 5), new Date(2020, 1, 1, 0, 0, 0, 0), new Attributes(false, false, false, false), "./assets/Images/cellar.jpg", 10.00),
-new Listing("TOP QUALITY Cellar by BESTCELLARS", new Location("3187 Reeves Dr", "Melvindale", "MI", "USA", 48122, new Geodata("point", new Coordinates(42.288179, -83.173806))), new Size(50, 50, 5), new Date(2019, 4, 5, 16, 8, 0, 0), new Attributes(false, false, false, false), "./assets/Images/dingycellar.jpg", 55.99),
-new Listing("Storage Unit", new Location("2850 N Pulaski Rd", "Chicago", "IL", "USA", 60641, new Geodata("point", new Coordinates(41.933241, -87.727606))), new Size(8, 8, 10), new Date(2020, 1, 4, 0, 0, 0, 0), new Attributes(false, false, false, false), "./assets/Images/storageunit.jpg", 5.00),
-new Listing("Back Room", new Location("5023 Conrad St", "Skokie", "IL", "USA", 60077, new Geodata("point", new Coordinates(42.037937, -87.752990))), new Size(10, 10, 8), new Date(2020, 1, 15, 8, 30, 0, 0), new Attributes(false, false, true, false), "./assets/Images/storeroom.jpg", 2.00),
-new Listing("Miniature Storage", new Location("2145 Sheridan Rd", "Evanston", "IL", "USA", 60208, new Geodata("point", new Coordinates(42.058053, -87.676167))), new Size(1, 2, 1), new Date(2020, 1, 14, 12, 0, 0, 0), new Attributes(false, false, false, false), "./assets/Images/mousehole.jpg", 0.50)];
-
-const JSONTestData = JSON.stringify(TestData);
-
-console.log(JSONTestData);
 
 const ListingContext = React.createContext();
 
@@ -94,7 +27,7 @@ function sizeCalculator(sizeObject) {
   return "Small";
 }
 
-function App() {
+const App = () => {
 
   const [currListing, updateCurrListing] = useState(null);
   function updateListing(newListing) {
@@ -122,8 +55,6 @@ const DetailView = () => {
       <Modal active={currListing != null} style={{ width: "100%", height: "100%", margin: 0 }}>
         <Modal.Background style={{ height: "100%", margin: "0px" }}></Modal.Background>
 
-
-
         <Modal.Card style={{ width: "100%", top: "-5%" }}>
           <Modal.Card.Body style={{ width: "100%", padding: "0px", margin: "0px" }}>
 
@@ -132,7 +63,6 @@ const DetailView = () => {
               &#10005;
             </div>
             <Image src="https://i.pinimg.com/originals/6a/7c/fc/6a7cfc513ee281ac19ed5b25f17a9a5a.jpg" style={{ width: "100%", padding: "0px" }} />
-
 
             {/* bottom bar */}
             <div style={{ height: "8%", backgroundColor: 'white', position: "fixed", bottom: "0", width: "100%", boxShadow: "0px -1px #888888" }}>
@@ -147,8 +77,6 @@ const DetailView = () => {
                       <span style={{ fontSize: "13px", paddingLeft: "2px" }}>(32)</span>
                     </div>
 
-
-
                   </div>
                 </th>
                 <th style={{ width: "42%", textAlign: "left", fontWeight: "normal", fontSize: "15px", padding: "6% 0" }}>
@@ -157,7 +85,6 @@ const DetailView = () => {
               </table>
 
             </div>
-
 
             {/* Content */}
             <Content style={{ width: "94%", margin: "auto", paddingTop: "1%", paddingBottom: "2%" }}>
@@ -207,6 +134,7 @@ const DetailView = () => {
 };
 
 const StorageCard = ({ listing }) => {
+
   const { currListing, updateListing } = useContext(ListingContext);
 
   return (
@@ -245,7 +173,6 @@ const StorageCard = ({ listing }) => {
 
           </div>
 
-
           <table style={{ width: "100%", marginTop: '10px' }}>
             <tr>
               <th style={{ width: "60%" }}>
@@ -267,15 +194,15 @@ const StorageCard = ({ listing }) => {
 };
 
 const ListingList = () => {
-  var Listings = JSON.parse(JSONTestData);
-  var columnIds = [...Array(Listings.length).keys()];
+  var listings = TestData;
+  var columnIds = [...Array(listings.length).keys()];
 
   return (
-    <div z-index="0" position="relative">
+    <div>
       <Column.Group multiline>
         {columnIds.map(i => (
           <Column key={i} size="one-quarter">
-            <StorageCard listing={Listings[i]} />
+            <StorageCard listing={listings[i]} />
           </Column>
         ))}
       </Column.Group>
