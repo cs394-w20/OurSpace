7ec1556fd5fcfeaae5c3f141a876738084c2af8d
Faraaz-Beyabani <beyabanifaraaz@gmail.com>
2020-01-18 16:36:57 -0600

    Added preliminary setup for viewing contact details

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useContext, useEffect } from "react";
 import "./App.css";
 import "rbx/index.css";
-import styles from "./assets/styles/DetailView.css"
+import "./assets/styles/DetailView.css"
 
 import parking from "./assets/icons/local_parking-24px.svg";
 import elevator from "./assets/icons/elevator.svg";
@@ -30,9 +30,10 @@ const App = () => {
 
   const [currListing, updateCurrListing] = useState(null);
   const [listingList, updateList] = useState([]);
+  const [contactViewOpen, toggleContactView] = useState(false);
 
   useEffect(() => {
-    function getListingsData () {
+    function getListingsData() {
       fetch('http://localhost:4000/get_listings', {
         method: 'POST',
         headers: {
@@ -40,120 +41,117 @@ const App = () => {
           'Content-Type': 'application/json'
         },
         credentials: 'include',
-        body: JSON.stringify({latitude:42.055984, longitude:-87.675171, listingsPerPage:10, pageNumber:1})
+        body: JSON.stringify({ latitude: 42.055984, longitude: -87.675171, listingsPerPage: 10, pageNumber: 1 })
       })
-      .then(response => response.json())
-      .then(response => {
-        updateList(response.listings);
-      });
+        .then(response => response.json())
+        .then(response => {
+          updateList(response.listings);
+        });
     }
     getListingsData();
   }, []);
 
-  function updateListing(newListing) {
-    updateCurrListing(newListing);
-  }
-  function updateAll(newList) {
+  const updateAll = (newListing) => {
     /* 
         BACKEND: ADD CODE HERE TO SET UP PUSHING NEW LISTINGS TO REMOTE DB
     */
-    updateList(newList);
+    updateList([newListing].concat(listingList))
   }
 
 
   return (
-    <ListingContext.Provider value={{ currListing, updateListing, listingList, updateList }}>
-      <div className="App" width="100%" height="100%" opacity="0.99">
-        <input z-index="0"></input>
-        <DetailView />
+    <ListingContext.Provider value={{ currListing, updateCurrListing, listingList, updateAll, contactViewOpen, toggleContactView }}>
+      <div className="App" width="100%" height="100%">
         <ListingList />
+        <DetailView />
+        <ContactView />
       </div>
     </ListingContext.Provider>
   );
 };
 
 const DetailView = () => {
-  const { currListing, updateListing } = useContext(ListingContext);
+  const { currListing, updateCurrListing, toggleContactView } = useContext(ListingContext);
 
   return (
     <div style={{ width: "100%", height: "100%", margin: 0 }}>
       <Modal active={currListing != null} id="dtView">
         {currListing != null ?
-        <React.Fragment>
-          <Modal.Background style={{ height: "100%", margin: "0px" }}></Modal.Background>
+          <React.Fragment>
+            <Modal.Background style={{ height: "100%", margin: "0px" }}></Modal.Background>
 
-          <Modal.Card style={{ width: "100%", height:"100%", top: "-5%" }}>
-            <Modal.Card.Body style={{ width: "100%", padding: "0px", margin: "0px" }}>
+            <Modal.Card style={{ width: "100%", height: "100%", top: "-5%" }}>
+              <Modal.Card.Body style={{ width: "100%", padding: "0px", margin: "0px" }}>
 
-              {/* Top exit icon */}
-              <div style={{ fontSize: '24px', color: 'white', position: "fixed", top: "1%", left: "3%" }} onClick={() => {document.getElementById("dtView").classList.remove("show"); setTimeout(function(){updateListing(null)}, 200)}}>
-                &#10005;
-              </div>
-              <Image src="https://i.pinimg.com/originals/6a/7c/fc/6a7cfc513ee281ac19ed5b25f17a9a5a.jpg" style={{ width: "100%", padding: "0px" }} />
+                {/* Top exit icon */}
+                <div style={{ fontSize: '24px', color: 'white', position: "fixed", top: "1%", left: "3%" }} onClick={() => { document.getElementById("dtView").classList.remove("show"); setTimeout(function () { updateCurrListing(null) }, 150) }}>
+                  &#10005;
+                </div>
+                <Image src="https://i.pinimg.com/originals/6a/7c/fc/6a7cfc513ee281ac19ed5b25f17a9a5a.jpg" style={{ width: "100%", padding: "0px" }} />
 
-              {/* bottom bar */}
-              <div style={{ height: "9%", backgroundColor: 'white', position: "fixed", bottom: "0", width: "100%", boxShadow: "0px -1px #888888" }}>
+                {/* bottom bar */}
+                <div style={{ height: "9%", backgroundColor: 'white', position: "fixed", bottom: "0", width: "100%", boxShadow: "0px -1px #888888" }}>
 
-                <table style={{ width: "100%" }}>
-                  <th style={{ width: "58%" }}>
-                    <div style={{ fontSize: '14px', textAlign: "left", fontWeight: "normal", margin: "4% 6%" }}>
+                  <table style={{ width: "100%" }}>
+                    <th style={{ width: "58%" }}>
+                      <div style={{ fontSize: '14px', textAlign: "left", fontWeight: "normal", margin: "4% 6%" }}>
 
-                      <span style={{ fontSize: "18px", fontWeight: "bold" }}>${currListing ? currListing.price : ""}</span> / NIGHT <br></br>
-                      <div style={{ display: 'inline-block' }}>
-                        <span style={{ color: 'green' }}>&#9733; 4.37</span>
-                        <span style={{ fontSize: "13px", paddingLeft: "2px" }}>(32)</span>
+                        <span style={{ fontSize: "18px", fontWeight: "bold" }}>${currListing ? currListing.price : ""}</span> / NIGHT <br></br>
+                        <div style={{ display: 'inline-block' }}>
+                          <span style={{ color: 'green' }}>&#9733; 4.37</span>
+                          <span style={{ fontSize: "13px", paddingLeft: "2px" }}>(32)</span>
+                        </div>
+
                       </div>
+                    </th>
+                    <th style={{ width: "42%", textAlign: "left", fontWeight: "normal", fontSize: "15px", padding: "6% 0" }}>
+                      <span style={{ backgroundColor: "	#4E2A84", padding: "10px", color: "white", fontWeight: "bold", borderRadius: "3px" }} onClick={() => { toggleContactView(true); setTimeout(function () { document.getElementById("ctView").classList.add("show") }, 100); }}>Check availability</span>
+                    </th>
+                  </table>
 
-                    </div>
-                  </th>
-                  <th style={{ width: "42%", textAlign: "left", fontWeight: "normal", fontSize: "15px", padding: "6% 0" }}>
-                    <span style={{ backgroundColor: "	#4E2A84", padding: "10px", color: "white", fontWeight: "bold", borderRadius: "3px" }}>Check availability</span>
-                  </th>
-                </table>
+                </div>
 
-              </div>
+                {/* Content */}
+                <Content style={{ width: "94%", margin: "auto", paddingTop: "1%", paddingBottom: "2%" }}>
+                  <Title>{currListing.name}</Title>
+                  <p>
+                    {currListing.description}
+                  </p>
 
-              {/* Content */}
-              <Content style={{ width: "94%", margin: "auto", paddingTop: "1%", paddingBottom: "2%" }}>
-                <Title>{currListing.name}</Title>
-                <p>
-                  {currListing.description}
+                  <p style={currListing.attributes.hasElevator || currListing.attributes.hasParking ? { height: '24px', fontWeight: "700", marginBottom: "5px" } : { display: "none" }}>Amenities</p>
+
+                  <table style={{ width: "100%" }}>
+                    <tr style={currListing.attributes.hasElevator ? { height: '22px' } : { display: "none" }}>
+                      <th style={{ width: "10%" }}>
+                        <img src={elevator} />
+                      </th>
+                      <th style={{ width: "90%", textAlign: "left", fontWeight: "normal", fontSize: "15px" }}>
+                        <p style={{ fontWeight: "500" }}>Elevator</p>
+                      </th>
+                    </tr>
+
+                    <tr style={currListing.attributes.hasParking ? { height: '22px' } : { display: "none" }}>
+                      <th style={{ width: "10%" }}>
+                        <img src={parking} />
+                      </th>
+                      <th style={{ width: "90%", textAlign: "left", fontWeight: "normal", fontSize: "15px" }}>
+                        <p style={{ fontWeight: "500" }}>Free parking on premise</p>
+                      </th>
+                    </tr>
+                  </table>
+
+
+                  <p>
+                    Dimensions: {currListing.size.length} x {currListing.size.width} x {currListing.size.height} ft
                 </p>
-
-                <p style={currListing.attributes.hasElevator || currListing.attributes.hasParking ? { height: '24px', fontWeight:"700", marginBottom:"5px" } : { display: "none" }}>Amenities</p>
-
-                <table style={{ width: "100%" }}>
-                  <tr style={currListing.attributes.hasElevator ? { height: '22px' } : { display: "none" }}>
-                    <th style={{ width: "10%" }}>
-                      <img src={elevator}/>
-                    </th>
-                    <th style={{ width: "90%", textAlign: "left", fontWeight: "normal", fontSize: "15px" }}>
-                      <p style={{fontWeight:"500"}}>Elevator</p>
-                    </th>
-                  </tr>
-
-                  <tr style={currListing.attributes.hasParking ? { height: '22px' } : { display: "none" }}>
-                    <th style={{ width: "10%" }}>
-                      <img src={parking}/>
-                    </th>
-                    <th style={{ width: "90%", textAlign: "left", fontWeight: "normal", fontSize: "15px" }}>
-                      <p style={{fontWeight:"500"}}>Free parking on premise</p>
-                    </th>
-                  </tr>
-                </table>
-                
-
-                <p>
-                  Dimensions: {currListing.size.length} x {currListing.size.width} x {currListing.size.height} ft
-                </p>
-                <p>
-                  Available until {(new Date(currListing.time)).toDateString()}
-                </p>
-              </Content>
-            </Modal.Card.Body>
-          </Modal.Card>
-        </React.Fragment>
-        : null}
+                  <p>
+                    Available until {(new Date(currListing.time)).toDateString()}
+                  </p>
+                </Content>
+              </Modal.Card.Body>
+            </Modal.Card>
+          </React.Fragment>
+          : null}
       </Modal>
     </div>
   );
@@ -161,10 +159,10 @@ const DetailView = () => {
 
 const StorageCard = ({ listing }) => {
 
-  const { updateListing } = useContext(ListingContext);
+  const { updateCurrListing } = useContext(ListingContext);
 
   return (
-    <div style={{ width: "90%", margin: "Auto", paddingTop: '20px' }} onClick={() => {updateListing(listing); setTimeout(function(){ document.getElementById("dtView").classList.add("show") }, 0);}}>
+    <div style={{ width: "90%", margin: "Auto", paddingTop: '20px' }} onClick={() => { updateCurrListing(listing); setTimeout(function () { document.getElementById("dtView").classList.add("show") }, 0); }}>
       <Card style={{ borderRadius: "8px" }}>
         <Card.Image>
           <Image.Container size="4by3">
@@ -184,7 +182,7 @@ const StorageCard = ({ listing }) => {
 
               <div style={{ display: 'inline-block' }}>
                 <span style={{ color: 'red' }}>&#9733;</span>{listing.rating.score}
-                  <span style={{ fontSize: "13px", paddingLeft: "2px" }}>({listing.rating.numRatings})</span>
+                <span style={{ fontSize: "13px", paddingLeft: "2px" }}>({listing.rating.numRatings})</span>
               </div>
             </th>
           </table>
@@ -220,7 +218,7 @@ const StorageCard = ({ listing }) => {
 
 const ListingList = () => {
 
-  const { listingList, updateAll } = useContext(ListingContext);
+  const { listingList } = useContext(ListingContext);
 
   var columnIds = [...Array(listingList.length).keys()];
 
@@ -236,5 +234,35 @@ const ListingList = () => {
     </div>
   );
 };
+
+const ContactView = () => {
+
+  // FRONTEND: Eventually fetch seller data from currListing.sellar or something
+
+  const { currListing, contactViewOpen, toggleContactView } = useContext(ListingContext);
+
+  return (
+    <div>
+      <Modal active={contactViewOpen} id="ctView">
+        {currListing != null ?
+          <React.Fragment>
+            <Modal.Background style={{ height: "100%", margin: "0%" }}></Modal.Background>
+
+            <Modal.Card style={{ width: "100%", height: "100%", top: "-5%" }}>
+              <Modal.Card.Body>
+                <div style={{ fontSize: '24px', color: 'white', position: "fixed", top: "1%", left: "3%" }} onClick={() => { document.getElementById("ctView").classList.remove("show"); setTimeout(function () { toggleContactView(false) }, 150); }}>
+                  &#10005;
+                </div>
+                <br/>
+                Poster: John Doe
+                Until: {new Date(currListing.time).toDateString()}
+              </Modal.Card.Body>
+            </Modal.Card>
+          </React.Fragment>
+          : null}
+      </Modal>
+    </div>
+  );
+}
 
 export default App;
